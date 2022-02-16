8e437c727d733090766c4883d1044f4415050e54
filius-fall <sreeram596@gmail.com>
2022-02-16 13:03:04 +0530

    setup

---
+++ run.py
@@ -0,0 +1,15 @@
+import os
+import discord
+
+DISCORD_TOKEN = os.environ.get("DISCORD_TOKEN")
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+
+client = MyClient()
+client.run(DISCORD_TOKEN)
--- test.py
+++
@@ -1,9 +0,0 @@
-from .video_downloader import twitch_vod_download,audio_download
-
-# twitch_vod_download('/home/filius-fall/', 'https://www.twitch.tv/videos/1248219017', 'test.mp4', '00:10:10', '00:12:00')
-audio_download('/home/filius-fall/Downloads', 'https://www.youtube.com/watch?v=YzxYnDgbsh4')
-print("succesful..................")
-
-
-
-# if __name__ == "__main__":
